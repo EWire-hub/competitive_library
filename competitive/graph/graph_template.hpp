30b55e910acefce7060e980be2e48fb9f0444933
EWire-hub <tyudai81@gmail.com>
2023-08-20 09:33:04 +0900

    graph edit edge

--- competitive/graph/graph_template.hpp
+++ competitive/graph/graph_template.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+template<class T = long long>
+struct Edge {
+    int to;
+    T cost;
+    int idx;
+
+    Edge() = default;
+    Edge(int to, T cost = 1, int idx = -1) : to(to), cost(cost), idx(idx) {}
+
+    operator int() const {return to;}
+};
