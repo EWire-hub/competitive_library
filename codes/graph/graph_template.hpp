080f4420f585a170706bd71abaee06e8a9631275
EWire-hub <tyudai81@gmail.com>
2023-08-20 15:11:01 +0900

    graph template

--- codes/graph/graph_template.hpp
+++ codes/graph/graph_template.hpp
@@ -9,10 +9,22 @@ struct Edge
 
     Edge() = default;
     Edge(int to, T cost = 1, int idx = -1) : to(to), cost(cost), idx(idx) {}
-
-    operator int() const { return to; }
 };
 
-struct Graph{
+template <class T = long long>
+struct Graph
+{
+    vector<vector<Edge>> g;
+    int es;
 
+    Graph() = default;
+    explicit Graph(int n) : g(n), es(0) {}
+
+    size_t size() const { return g.size(); }
+    void add_edge(int from, int to, T cost = 1, bool direction = false)
+    {
+        if (direction)
+            g[to].emplace_back(from, cost, es);
+        g[from].emplace_back(to, cost, es++);
+    }
 };
