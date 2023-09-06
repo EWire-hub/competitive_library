ea9d5896ac6ee3a8c45a3ff17d1ddcff05215ec9
EWire-hub <tyudai81@gmail.com>
2023-09-06 17:09:18 +0900

    fix format

--- competitive/math/number_theory/is_prime.hpp
+++ competitive/math/number_theory/is_prime.hpp
@@ -1,7 +1,7 @@
-bool is_prime(long long x){
-    if(x <= 1) return false;
-    for(long long i = 2; i * i <= x; i++){
-        if(x % i == 0) return false;
+bool is_prime(long long x) {
+    if (x <= 1) return false;
+    for (long long i = 2; i * i <= x; i++) {
+        if (x % i == 0) return false;
     }
     return true;
 }
